1fbdce4f23c439416c8bba6c84bff41498f675ed
Tobias Løfgren <tobloef@gmail.com>
2024-06-25 22:13:02 +0200

    Add build events system

---
+++ src/events.js
@@ -0,0 +1,70 @@
+export const buildEvents = {
+	/** @type {BuildEventType<string>} */
+	fileAdded: createBuildEventType(),
+	/** @type {BuildEventType<string>} */
+	fileUpdated: createBuildEventType(),
+	/** @type {BuildEventType<string>} */
+	fileRemoved: createBuildEventType(),
+	/** @type {BuildEventType<void>} */
+	reloadRequested: createBuildEventType(),
+}
+
+
+/**
+ * @template T
+ * @typedef {Object} BuildEventType
+ * @property {(listener: (event: BuildEvent<T>) => void) => Unsubscribe} subscribe
+ * @property {(event: T) => void} publish
+ */
+
+/**
+ * @typedef {() => void} Unsubscribe
+ */
+
+/**
+ * @template T
+ * @typedef {Object} BuildEvent
+ * @property {() => void} stopPropagation
+ * @property {boolean} isPropagationStopped
+ * @property {T} data
+ */
+
+/**
+ * @template T
+ * @returns {BuildEventType<T>}
+ */
+function createBuildEventType() {
+	/** @type {Record<number, (event: BuildEvent<T>) => void>} */
+	const listeners = {};
+
+	let nextId = 0;
+
+	return {
+		subscribe: (listener) => {
+			const id = nextId++;
+
+			listeners[id] = listener;
+
+			return () => {
+				delete listeners[id];
+			}
+		},
+		publish: (data) => {
+			const event = {
+				stopPropagation: () => {
+					event.isPropagationStopped = true;
+				},
+				data,
+				isPropagationStopped: false,
+			};
+
+			for (const listener of Object.values(listeners)) {
+				listener(event);
+
+				if (event.isPropagationStopped) {
+					break;
+				}
+			}
+		},
+	}
+}
