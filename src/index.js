af0cd17774ae222d1a8975bf4bced41101df16e0
Tobias Løfgren <tobloef@gmail.com>
2024-08-04 18:49:03 +0200

    Fix import order

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-export * from "./presets/index.js";
-export * from "./module/index.js";
 export { BuildConfig } from "./build-config.js";
+export * from "./module/index.js";
+export * from "./presets/index.js";
 
 
 
